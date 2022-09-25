033b7183fee492baa9da59c98d4e9aabe3cfd64f
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-09-24 23:59:09 -0400

    created a react app with very rough nav and hero

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import 'bootstrap'
+import '../index.css'
+
+const Hero = () => {
+return(
+        <div 
+        className='p-5 text-center bg-image jumbotron-image'
+        style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: 500 }}
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)', height: 400 }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 id="test" className='mb-3'>Nakia McNeil</h1>
+              <h4 className='mb-3'>Aspiring Graphic Designer</h4>
+              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+                See My Designs
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+)
+}
+
+export default Hero;
